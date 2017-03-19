31f6a91d32a5c07945b62822a3e93db5e55679c5
Dane Bertram <dane@articulate.com>
2017-03-19 17:48:06 -0400

    WIP: automatically constructed SUT is disposed via EnsureSUT/SUT = SUT

---
+++ SpecEasy.Specs/TinyIoC/TinyIoCSUTDisposal.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using NUnit.Framework;
+
+namespace SpecEasy.Specs.TinyIoC
+{
+    internal sealed class TinyIoCSUTDisposal : Spec<DisposableSubscriber>
+    {
+        public void SUTDisposal()
+        {
+            When("the updateable is updated", () => Raise<IUpdateable>(updateable => updateable.Updated += null, Get<IUpdateable>(), EventArgs.Empty));
+
+            Given("SUT is constructed automatically", () => EnsureSUT()).Verify(() =>
+                Then("the updated count should be 1", () => Assert.AreEqual(1, SUT.UpdatedCount)).
+                Then("SUT has not been disposed", () => Assert.IsFalse(SUT.Disposed)));
+
+            Given("SUT is constructed explicitly", () => SUT = new DisposableSubscriber(Get<IUpdateable>())).Verify(() =>
+                Then("the updated count should be 1", () => Assert.AreEqual(1, SUT.UpdatedCount)).
+                Then("SUT has not been disposed", () => Assert.IsFalse(SUT.Disposed)));
+        }
+    }
+}
